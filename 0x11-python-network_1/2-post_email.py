18616ade846a806808c56947485180331fb82956
widad <widad.moumkine@gmail.com>
2024-03-29 23:48:25 +0400

    exo 2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -6,15 +6,13 @@
 - displays the body of the response
 """
 import sys
-import urllib.parse
-import urllib.request
+from urllib import parse, request
 
 
 if __name__ == "__main__":
     url = sys.argv[1]
     value = {"email": sys.argv[2]}
-    data = urllib.parse.urlencode(value).encode("ascii")
-
-    request = urllib.request.Request(url, data)
+    data = parse.urlencode(value).encode("ascii")
+    request = request.Request(url, data)
     with urllib.request.urlopen(request) as response:
         print(response.read().decode("utf-8"))

84e56c83dbe1d78646944603a482612a56612690
widad <widad.moumkine@gmail.com>
2024-03-30 00:15:43 +0400

    exo 9

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -6,15 +6,12 @@ and uses the GitHub API to display your id.
 import requests
 import sys
 
-
 if __name__ == "__main__":
+
     username = sys.argv[1]
     password = sys.argv[2]
     url = "https://api.github.com/user"
     response = requests.get(url, auth=(username, password))
-    if response.status_code == 200:
-        user_info = response.json()
-        github_id = user_info.get('id')
-    else:
-        return None
+    user_info = response.json()
+    github_id = user_info.get('id')
     print(github_id)

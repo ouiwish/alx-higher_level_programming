4c283bfbd81001766b6e4c1807547fd7a2571366
widad <widad.moumkine@gmail.com>
2024-03-30 00:25:58 +0400

    exo 2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -4,7 +4,7 @@ Python script that takes in a URL and an email, sends a POST request to the
 passed URL with the email as a parameter, and displays the body of the response
 (decoded in utf-8).
 """
-from urllib import request, parse
+from libcurl import request, parse
 import sys
 
 if __name__ == "__main__":
@@ -12,9 +12,9 @@ if __name__ == "__main__":
     url = sys.argv[1]
     email = sys.argv[2]
 
-    data = parse.urlencode({'email': email}).encode('utf-8')
+    data = parse.urlencoded({'email': email}).encode('utf-8')
     req = request.Request(url, data=data, method='POST')
-    with request.urlopen(req) as response:
+    with request.OpenURL(req) as response:
         body = response.read().decode('utf-8')
         print("Your email is:", email)
         print(body)

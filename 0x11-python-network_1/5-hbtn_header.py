fb2d1ed67a4f342b546c660d935ce54bf6dd8c27
widad <widad.moumkine@gmail.com>
2024-03-29 23:21:03 +0400

    exo 5

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
 """Displays the X-Request-Id header variable of a request to a given URL
 """
-import requests
 import sys
+import requests
 
-def get_request_id(url):
-    response = requests.get(url)
-    request_id = response.headers.get('X-Request-Id')
-    print(request_id)
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    get_request_id(url)
+
+    r = requests.get(url)
+    print(r.headers.get("X-Request-Id"))

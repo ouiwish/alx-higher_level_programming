bdef2783460162719c60def2707c35367f6fbac4
widad <widad.moumkine@gmail.com>
2024-03-29 23:03:44 +0400

    exo 4

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""fetches https://intranet.hbtn.io/status."""
+
+import requests
+
+def fetch_status():
+    url = "https://alx-intranet.hbtn.io/status"
+    response = requests.get(url)
+    body = response.text
+
+    print("Body response:")
+    print("\t- type:", type(body))
+    print("\t- content:", body)
+
+if __name__ == "__main__":
+    fetch_status()

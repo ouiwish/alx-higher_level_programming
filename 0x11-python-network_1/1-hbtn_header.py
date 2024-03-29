ff45d8ed10d7c45d0a6ae67c2247961094a51c69
widad <widad.moumkine@gmail.com>
2024-03-30 00:31:34 +0400

    exo 1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -11,6 +11,6 @@ if __name__ == "__main__":
 
     url = sys.argv[1]
 
-    with urllib.request.urlopen(url) as response:
+    with request.urlopen(url) as response:
         x_request_id = response.getheader('X-Request-Id')
         print(x_request_id)

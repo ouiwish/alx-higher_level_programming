a40b671a4a1b43ac6e027544b6bc195ab081c1bc
widad <widad.moumkine@gmail.com>
2024-03-30 00:06:34 +0400

    exo 7

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -7,16 +7,12 @@ to 400, it prints: Error code: followed by the value of the HTTP status code.
 import requests
 import sys
 
-def fetch_url(url):
-    """
-    Fetches the URL and displays the body of the response or prints an error code.
-    """
+if __name__ == "__main__":
+
+    url = sys.argv[1]
+
     response = requests.get(url)
     if response.status_code >= 400:
         print(f"Error code: {response.status_code}")
     else:
         print(response.text)
-
-if __name__ == "__main__":
-    url = sys.argv[1]
-    fetch_url(url)

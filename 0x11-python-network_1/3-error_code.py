006fe2967baefac10f2d2f3af3d875e100fa7109
widad <widad.moumkine@gmail.com>
2024-03-29 23:42:45 +0400

    exo 3

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -19,4 +19,9 @@ def fetch_url(url):
 if __name__ == "__main__":
 
     url = sys.argv[1]
-    fetch_url(url)
+    try:
+        with urllib.request.urlopen(url) as response:
+            body = response.read().decode('utf-8')
+            print(body)
+    except urllib.error.HTTPError as e:
+        print(f"Error code: {e.code}")

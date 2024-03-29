5d4319d739a9af7c8fcc1d9ea689c1ac2ab63fd2
widad <widad.moumkine@gmail.com>
2024-03-29 23:43:39 +0400

    exo 3

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -8,14 +8,6 @@ import urllib.request
 import urllib.error
 import sys
 
-def fetch_url(url):
-    try:
-        with urllib.request.urlopen(url) as response:
-            body = response.read().decode('utf-8')
-            print(body)
-    except urllib.error.HTTPError as e:
-        print(f"Error code: {e.code}")
-
 if __name__ == "__main__":
 
     url = sys.argv[1]

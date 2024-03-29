3d6beb599999f8e4f15ba02cb56fda2f3430cf3e
widad <widad.moumkine@gmail.com>
2024-03-30 00:42:33 +0400

    exo 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,12 +2,11 @@
 """
 Python script that fetches https://alx-intranet.hbtn.io/status using urllib.
 """
+
 import urllib.request
 
-def fetch_status():
-    """
-    Fetches the URL and displays the body of the response.
-    """
+if __name__ == "__main__":
+
     url = "https://alx-intranet.hbtn.io/status"
     with urllib.request.urlopen(url) as response:
         body = response.read()
@@ -15,6 +14,3 @@ def fetch_status():
         print("\t- type:", type(body))
         print("\t- content:", body)
         print("\t- utf8 content:", body.decode('utf-8'))
-
-if __name__ == "__main__":
-    fetch_status()

ae5fc120e616629c7c21a22d691701056e3e8018
widad <widad.moumkine@gmail.com>
2024-03-30 00:24:56 +0400

    exo 2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -6,7 +6,9 @@ passed URL with the email as a parameter, and displays the body of the response
 """
 from urllib import request, parse
 import sys
+
 if __name__ == "__main__":
+
     url = sys.argv[1]
     email = sys.argv[2]
 

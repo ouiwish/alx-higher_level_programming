3a74cbc575b0e587581b1e92bbb4317ac523e418
widad <widad.moumkine@gmail.com>
2024-03-29 23:39:39 +0400

    exo 3

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,13 +1,21 @@
 #!/usr/bin/python3
-"""error code"""
-from urllib import request, error
+"""A script that:
+- takes in a URL,
+- sends a request to the URL
+- displays the body of the response (decoded in utf-8).
+"""
+import urllib.request
+import urllib.error
 import sys
+
+def fetch_url(url):
+    try:
+        with urllib.request.urlopen(url) as response:
+            body = response.read().decode('utf-8')
+            print(body)
+    except urllib.error.HTTPError as e:
+        print(f"Error code: {e.code}")
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    try:
-        with urllib.request.urlopen(url) as response:
-             body = response.read().decode('utf-8')
-             print(body)
-        except urllib.error.HTTPError as e:
-            print(f"Error code: {e.code}")
+    fetch_url(url)

4c74599a60147eaed7736b96a92e93465b73a935
widad <widad.moumkine@gmail.com>
2024-03-29 23:31:15 +0400

    exo 3

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 """error code"""
+import sys
+from urllib import request, error
 
 if __name__ == "__main__":
-    import sys
-    from urllib import request, error
-
     try:
         with request.urlopen(sys.argv[1]) as res:
-            print(res.read().decode('UTF-8'))
+        print(res.read().decode('UTF-8'))
     except error.HTTPError as er:
         print('Error code:', er.code)
         print(er.read().decode('UTF-8'))  # Print the error message as well

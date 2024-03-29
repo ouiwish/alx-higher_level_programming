3f7226e3d3aa2e0a2ea02056b2678a697a09eb56
widad <widad.moumkine@gmail.com>
2024-03-29 23:29:06 +0400

    exo 4

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -2,14 +2,10 @@
 """fetches https://intranet.hbtn.io/status"""
 import requests
 
-def fetch_status():
-    url = "https://alx-intranet.hbtn.io/status"
+if __name__ == "__main__":
+   url = "https://alx-intranet.hbtn.io/status"
     response = requests.get(url)
     body = response.text
-
     print("Body response:")
     print("\t- type:", type(body))
     print("\t- content:", body)
-
-if __name__ == "__main__":
-    fetch_status()

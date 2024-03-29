9472e2beff9a347dbda43dc06eab532e16638bb5
widad <widad.moumkine@gmail.com>
2024-03-30 00:32:27 +0400

    exo 1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
+"""A script that:
+- takes in a URL,
+- sends a request to the URL and displays the value
+- of the X-Request-Id variable found in the header ofthe response.
 """
-Python script that takes in a URL, sends a request to the URL, and displays
-the value of the X-Request-Id variable found in the header of the response.
-"""
-from  urllib import request
 import sys
-
+import urllib.request
 
 if __name__ == "__main__":
-
     url = sys.argv[1]
 
-    with request.urlopen(url) as response:
-        x_request_id = response.getheader('X-Request-Id')
-        print(x_request_id)
+    request = urllib.request.Request(url)
+    with urllib.request.urlopen(request) as response:
+        print(dict(response.headers).get("X-Request-Id"))

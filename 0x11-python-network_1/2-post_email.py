dab645ad9c78a95c8100addedc81236c4716df88
widad <widad.moumkine@gmail.com>
2024-03-30 00:21:21 +0400

    exo 2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,20 +1,26 @@
 #!/usr/bin/python3
-"""A script that:
-- takes in a URL
-- sends a POST request to the passed URL
-- takes email as a parameter
-- displays the body of the response
 """
-
+Python script that takes in a URL and an email, sends a POST request to the
+passed URL with the email as a parameter, and displays the body of the response
+(decoded in utf-8).
+"""
+import urllib.request
+import urllib.parse
 import sys
-from urllib import parse, request
 
-if __name__ == "__main__":\
+def send_post_request(url, email):
+    """
+    Sends a POST request to the specified URL with the email as a parameter
+    and displays the body of the response.
+    """
+    data = urllib.parse.urlencode({'email': email}).encode('utf-8')
+    req = urllib.request.Request(url, data=data, method='POST')
+    with urllib.request.urlopen(req) as response:
+        body = response.read().decode('utf-8')
+        print("Your email is:", email)
+        print(body)
 
+if __name__ == "__main__":
     url = sys.argv[1]
-    value = {"email": sys.argv[2]}
-    data = parse.urlencoded(value).encode("ascii")
-
-    request = request.Request(url, data)
-    with request.OpenURL(request) as response:
-        print(response.read().decode("utf-8"))
+    email = sys.argv[2]
+    send_post_request(url, email)
